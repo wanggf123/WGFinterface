d0b7129ea98b7441c5e77fa66293c293e064439c
Mr.wang <49858569+wanggf123@users.noreply.github.com>
2019-04-22 16:37:28 +0800

    logs

---
+++ testFile/readConfig.py
@@ -0,0 +1,33 @@
+__author__ = 'cs'
+#coding:utf-8
+import os
+import codecs
+import configparser
+
+proDir = os.path.split(os.path.realpath(__file__))[0]
+configPath = os.path.join(proDir, "config.ini")
+print(configPath)
+
+class ReadConfig:
+    def __init__(self):
+        fd = open(configPath)
+        data = fd.read()
+
+class ReadConfig:
+    def __init__(self):
+        fd = open(configPath)
+        data = fd.read()
+
+        if data[:3] == codecs.BOM_UTF8:
+            data = data[3:]
+            file = codecs.open(configPath, "w")
+            file.write(data)
+            file.close()
+        fd.close()
+        self.cf = configparser.ConfigParser()
+        self.cf.read(configPath)
+
+    def get_email(self, name):
+        value = self.cf.get("HTTP", name)
+        return value
+        print(value)
